b122960152d6cc986001f05834d7df4663606760
AlixSil <alix.silvert@protonmail.com>
2023-02-20 15:23:20 +0100

    Debuged TMM, added Rank

--- preprocessing/rnanormaliser.py
+++ preprocessing/rnanormaliser.py
@@ -118,6 +118,8 @@ class TMM(BaseEstimator, TransformerMixin):
                 self.scaled_reference_sample = (
                     X[reference_line] / library_sizes[reference_line]
                 )
+                
+        return(self)
 
     def transform(self, X, library_sizes=None, **transform_params):
         if type(X) == pd.DataFrame:
@@ -128,28 +130,42 @@ class TMM(BaseEstimator, TransformerMixin):
         if library_sizes is None:
             library_sizes = X.sum(axis=1)
 
+
+        reference_non_zero = np.nonzero(self.scaled_reference_sample)[0]
+
+
         for s in range(X_copy.shape[0]):
+
             # loop on sample
+            current_sample = X_copy[s]
             scaled_current_sample = current_sample / library_sizes[s]
 
-            M = np.log2(self.scaled_reference_sample) - np.log2(scaled_current_sample)
+            current_sample_non_zero_index = np.nonzero(scaled_current_sample)[0]
+
+            non_zero_indexes = np.intersect1d(reference_non_zero, current_sample_non_zero_index)
+
+
+            M = np.log2(self.scaled_reference_sample[non_zero_indexes] / scaled_current_sample[non_zero_indexes])
             A = (
-                np.log(self.scaled_reference_sample)
-                + np.log(self.scaled_reference_sample)
+                np.log2(self.scaled_reference_sample[non_zero_indexes] * scaled_current_sample[non_zero_indexes])
             ) / 2
 
             m_lower, m_higher = np.percentile(M, [30, 70])
             a_lower, a_higher = np.percentile(A, [5, 95])
 
+
             references_genes_index = np.where(
                 (M >= m_lower) & (M <= m_higher) & (A >= a_lower) & (A <= a_higher)
-            )
+            )[0]
+
+            print(references_genes_index)
 
             M_trimmed = M[references_genes_index]
-            scaled_current_sample_trimmed = scaled_current_sample[
+
+            scaled_current_sample_trimmed = scaled_current_sample[non_zero_indexes][
                 references_genes_index
             ]
-            scaled_reference_sample_trimmed = self.scaled_reference_sample[
+            scaled_reference_sample_trimmed = self.scaled_reference_sample[non_zero_indexes][
                 references_genes_index
             ]
 
@@ -159,8 +175,29 @@ class TMM(BaseEstimator, TransformerMixin):
                 (1 - scaled_reference_sample_trimmed) / scaled_reference_sample_trimmed
             )
 
-            tmm = 2 ** np.mean(M, weights=weights)
 
+            tmm = 2 ** np.average(M_trimmed, axis = None, weights=weights)
+
+
+            X_copy = X_copy.astype("float32")
             X_copy[s] = X_copy[s] / tmm
 
         return X_copy
+
+
+class RankedExpression(BaseEstimator, TransformerMixin):
+    """TODO"""
+
+    def fit(self, X, **fit_params): 
+        self.number_of_genes = X.shape[1]
+        return(self)
+
+    def transform(self, X, **transform_params):
+
+        if X.shape[1] == self.number_of_genes:
+            raise ValueError(
+                "X has a different number of genes than the set this normalizer was fitted on"
+            )
+
+        X_copy = X.argsort(axis=1)/argsort(axis=1)
+        return(X_copy)
---
+++ tests/test_TMM.py
@@ -0,0 +1,47 @@
+from preprocessing.rnanormaliser import TMM
+import pandas as pd
+import numpy as np
+import pytest
+import random
+
+@pytest.fixture
+def example_np_fitted_1():
+    test_np = np.array([
+       [1, 9, 8, 8, 7, 7, 1, 2, 0, 8, 5, 8, 4, 0, 1, 7, 6, 5, 3, 6],
+       [1, 2, 5, 1, 3, 4, 4, 2, 9, 0, 3, 7, 7, 8, 5, 1, 5, 4, 0, 5],
+       [6, 7, 7, 7, 0, 8, 2, 7, 2, 9, 6, 9, 6, 6, 7, 2, 3, 0, 5, 7],
+       [9, 9, 7, 3, 4, 6, 2, 5, 1, 2, 1, 4, 7, 5, 1, 1, 0, 3, 2, 9],
+       [3, 7, 6, 5, 6, 7, 5, 8, 5, 8, 1, 6, 4, 5, 1, 7, 0, 2, 9, 6],
+       [6, 6, 3, 8, 6, 9, 6, 2, 8, 8, 5, 5, 3, 4, 1, 5, 1, 0, 0, 2],
+       [4, 9, 2, 1, 2, 8, 4, 3, 5, 5, 4, 7, 8, 6, 1, 6, 6, 5, 7, 8],
+       [8, 2, 4, 3, 6, 1, 4, 0, 6, 9, 8, 9, 0, 3, 5, 7, 9, 0, 0, 0],
+       [1, 1, 3, 2, 9, 5, 6, 4, 0, 2, 0, 8, 3, 4, 3, 1, 5, 1, 1, 6],
+       [5, 5, 0, 5, 3, 4, 1, 9, 1, 6, 0, 9, 4, 0, 0, 2, 7, 2, 3, 5]
+       ])
+    #Reference sample should be the last by default
+    return(test_np)
+
+def test_normal_fitting(example_np_fitted_1):
+    test_np = example_np_fitted_1
+    nrm = TMM()
+    nrm.fit(test_np)
+
+    normaly_selected = np.array([5, 5, 0, 5, 3, 4, 1, 9, 1, 6, 0, 9, 4, 0, 0, 2, 7, 2, 3, 5])
+    normaly_selected = normaly_selected/normaly_selected.sum()
+
+    assert np.array_equal( nrm.scaled_reference_sample, normaly_selected)
+
+def test_fitting_choose_line_and_library_size(example_np_fitted_1):
+    test_np = example_np_fitted_1
+    library_sizes = np.array([1 for k in range(10)])
+
+
+    nrm = TMM()
+    nrm.fit(test_np, library_sizes = library_sizes, reference_line = 1)
+
+    assert np.array_equal(nrm.scaled_reference_sample, np.array([1, 2, 5, 1, 3, 4, 4, 2, 9, 0, 3, 7, 7, 8, 5, 1, 5, 4, 0, 5]))
+
+def test_fit_transform(example_np_fitted_1):
+    test_np = example_np_fitted_1
+    nrm = TMM()
+    nrm.fit_transform(test_np)
