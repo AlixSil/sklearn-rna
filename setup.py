0fad4a166c42ddaabfdab68afbd6ec1f417dd3ed
AlixSil <alix.silvert@protonmail.com>
2023-05-23 14:49:49 +0200

    first draft of setup file

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-setup.py
+from setuptools import find_packages, setup
+
+setup(
+    name='sklearn-rna',
+    packages=find_packages(include=['sklearnRNA']),
+    version='0.1.0',
+    description='Library offering tool for the treatment of RNA-seq data in the context of machine learning',
+    author='Alix Silvert',
+    license='MIT',
+    install_requires=["pandas", "numpy", "scikit-learn"],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
